b67ab75b47de1bf9c3a9ba91537dd56e79a6f25f
Filip Peterek <fpeterek@seznam.cz>
2019-11-11 12:58:32 +0100

    Boulder sprite (non-rotating)

--- src/main/java/AztecChallenge/Minigames/Stairs/Boulder.java
+++ src/main/java/AztecChallenge/Minigames/Stairs/Boulder.java
@@ -4,24 +4,25 @@ import AztecChallenge.GameEngine.GameEntity;
 import AztecChallenge.Interfaces.Damaging;
 import AztecChallenge.Interfaces.Renderable;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
 public class Boulder extends GameEntity implements Renderable, Damaging {
 
     public Color color;
+    private Image sprite;
 
-    public Boulder(double x, double y, double width, double height) {
+    public Boulder(double x, double y, double width, double height, Image sprite) {
         super(x, y, width, height);
-        color = new Color(0.5,0.3, 0.2, 1);
+        this.sprite = sprite;
         affectedByGravity(true);
     }
 
-
     @Override
     public void render(GraphicsContext gc) {
-        gc.setFill(Paint.valueOf(color.toString()));
-        gc.fillRect(x(), y(), width(), height());
+        int i = 0;
+        gc.drawImage(sprite, i * width(), 0, width(), height(), x(), y(), width(), height());
     }
 
 }
--- src/main/java/AztecChallenge/Minigames/Stairs/BoulderCreator.java
+++ src/main/java/AztecChallenge/Minigames/Stairs/BoulderCreator.java
@@ -1,5 +1,7 @@
 package AztecChallenge.Minigames.Stairs;
 
+import javafx.scene.image.Image;
+
 import java.util.Random;
 
 public class BoulderCreator {
@@ -9,15 +11,17 @@ public class BoulderCreator {
     private int from;
     private int spread;
     private Random rand;
+    private Image sprite;
 
     public BoulderCreator(int fromX, int toX) {
+        sprite = new Image("stairs_boulder.png", boulderSize * 4, boulderSize, true, false);
         from = fromX;
         spread = toX - fromX;
         rand = new Random();
     }
 
     Boulder createBoulder() {
-        Boulder b = new Boulder(from + rand.nextInt(spread), -boulderSize, boulderSize, boulderSize);
+        Boulder b = new Boulder(from + rand.nextInt(spread), -boulderSize, boulderSize, boulderSize, sprite);
         b.setForces(0, 5);
         return b;
     }
